7e976268b4cf1697bbee4b0c5c2ede996703f586
yanokasuuki-creator <yanokasuuki@gmail.com>
2025-12-28 01:31:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Professor', //  Valkyrie bot 
+  ownerNumber: '919876543210', //+918415057306Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
